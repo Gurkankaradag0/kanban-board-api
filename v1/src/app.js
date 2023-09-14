153a33d520c2ec3ecad76ba63539363cc33d6787
Gürkan Karadağ <gurkan.karadag.1907@gmail.com>
2023-09-14 21:43:39 +0300

    add cors

--- v1/src/app.js
+++ v1/src/app.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const helmet = require('helmet')
+const cors = require('cors')
 require('./config')()
 require('./loaders')()
 const { UserRoutes, SessionRoutes } = require('./api-routes')
@@ -7,6 +8,14 @@ const { UserRoutes, SessionRoutes } = require('./api-routes')
 const app = express()
 app.use(express.json())
 app.use(helmet())
+app.use(
+    cors({
+        origin: '*',
+        methods: 'GET,PATCH,POST,DELETE',
+        preflightContinue: false,
+        optionsSuccessStatus: 204
+    })
+)
 
 app.listen(process.env.APP_PORT, () => {
     console.log(`The server is running on port ${process.env.APP_PORT}...`)
